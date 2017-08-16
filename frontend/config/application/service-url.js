5b7fd7be3bcbae554a65ae5dd08e9d12513e4207
ostkzd1 <dmitry.kuznetsov>
2017-08-16 20:20:09 +0300

    change url api

--- frontend/config/application/service-url.js
+++ frontend/config/application/service-url.js
@@ -2,7 +2,7 @@ const urlService = {
   // Корень сервисов бакенда, должен оканчиваться на слеш "/"
   SERVICE_BASE_URL: process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000/api/'
-    : 'http://94.130.98.97/:3000/api/'
+    : 'http://94.130.98.97:3000/api/'
 }
 
 export default urlService
